91787132811361b7b0f53acfb09dcbb2adca5b84
Romain Schaff <romainschaff2612@gmail.com>
2023-03-04 19:17:06 +0100

    Correction page confirm gif

--- js/confirmation.js
+++ js/confirmation.js
@@ -5,7 +5,7 @@ main.innerHTML = `
 <p> Le numéro de la commande est : ${localStorage.orderId}</p>
 <br>
 <p> Prix total : ${localStorage.total}€</p>
-<iframe src="https://giphy.com/embed/IwAZ6dvvvaTtdI8SD5" width="480" height="400" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p>
+<iframe src="https://giphy.com/embed/IwAZ6dvvvaTtdI8SD5" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p>
 <a href="https://giphy.com/gifs/theoffice-the-office-tv-michaels-birthday-IwAZ6dvvvaTtdI8SD5">via GIPHY</a></p>
 </div>
 `;
